58991f19e618bbb77dd67580bb3f16c04deb4f24
wakahiu <wakahiu@berkeley.edu>
2015-11-12 19:26:47 -0500

    Setting up html server

--- backend/config/security.js
+++ backend/config/security.js
@@ -1,4 +1,4 @@
 // gitignore and do not check into source code control for obvious reasons.
 module.exports = {
-     stormpath_secret_key : ‘YOUR STORMPATH APPLICATION KEY GOES HERE’;
+     stormpath_secret_key : ‘YOUR STORMPATH APPLICATION KEY’;
 }
