fde421a018f09ee07349f634e8067f681ba7edd4
rabbit <qw2181@columbia.edu>
2015-12-09 10:20:22 -0500

    at least showing up something

--- android/RestaurantApp/app/src/main/java/restaurantapp/restaurantapp/stafforderactivity.java
+++ android/RestaurantApp/app/src/main/java/restaurantapp/restaurantapp/stafforderactivity.java
@@ -42,10 +42,14 @@ public class stafforderactivity extends ListActivity {
     // tmp hashmap for this specific order
     HashMap<String, String> order_tmpmap = new HashMap<String, String>();
     // Strings
-    String ID2look4 = "565a9ff2dcdbac2015e7b84a";
+    String ID2look4 = "";
+    String counterlimit = null;
+
     String IDfoundObjtxt, oitemname, oitemprice;
     // int
     int index = 0;
+    int counterint = 0;
+
 
     String emailtxt;
 
@@ -99,61 +103,75 @@ public class stafforderactivity extends ListActivity {
 
             Log.e("Response: ", "> " + jsonStr);
 
+//            if ( counterlimit != null )
+//                counterint = Integer.parseInt(counterlimit);
+
             if (jsonStr != null) {
-                try {
-                    Boolean compareresult = false;
-                    // initialize a JSONObject from the GET response entity
-                    JSONObject jsonObj = new JSONObject(jsonStr);
+                try { //counterint = 10;
+//                    for (int jk = 1; jk < counterint + 1; jk++) {
+//                        ID2look4 = getIntent().getStringExtra("chosenorderID" + jk);
+                        //
+                        Boolean compareresult = false;
+                        // initialize a JSONObject from the GET response entity
+                        JSONObject jsonObj = new JSONObject(jsonStr);
 
-                    // search within found JSONObject to get the array with (specific name)
-                    orderIDArray = jsonObj.getJSONArray("orders");
+                        // search within found JSONObject to get the array with (specific name)
+                        orderIDArray = jsonObj.getJSONArray("orders");
+                        //
+                        Log.d("orderID", orderIDArray.toString());
+                        //
+                        for (int i = 0; i < orderIDArray.length(); i++) {
+                            IDfoundObj = orderIDArray.getJSONObject(i);
+                            //
+                            Log.d("orderID Array", String.valueOf(orderIDArray.length()));
+                            //
+                            IDfoundObjtxt = IDfoundObj.get("_id").toString();
 
-                    for (int i = 0; i < orderIDArray.length(); i++) {
-                        IDfoundObj = orderIDArray.getJSONObject(i);
-                        //
-                        Log.d("1stArray", orderIDArray.toString());
-                        //
-                        IDfoundObjtxt = IDfoundObj.get("_id").toString();
-                        //
-                        Log.d("1stArrayString", IDfoundObjtxt);
-                        Log.d("ArrayStringCompare", ID2look4);
-                        //
-                        if (IDfoundObjtxt.equals(ID2look4)) {
-                            compareresult = true;
-                            index = i;
+                            //
+                            Log.d("orderID Array as String", IDfoundObjtxt);
+                            Log.d("ArrayString2Compare", ID2look4 + "");
+                            //
+                            if (IDfoundObjtxt.equals(ID2look4)) {
+                                compareresult = true;
+                                index = i;
+                            }
                         }
-                    }
-                    // if desired ID object is found
-                    if (compareresult.equals(true)) {
-                        IDorder = orderIDArray.getJSONObject(index);
-                        IDmenuitems = IDorder.getJSONArray("menuItems");
-                        //IDmenuitems = IDfoundObj.getJSONArray("menuItems");
-                        //
-                        Log.d("ID order Obj", IDorder.toString());
-                        Log.d("ID menuitem array", IDmenuitems.toString());
-                        //
-                        for (int j = 0; j < IDmenuitems.length(); j++) {
-                            JSONObject menuitemobjs = IDmenuitems.getJSONObject(j);
+                        // if desired ID object is found
+//                        if (compareresult.equals(true)) {
+                            IDorder = orderIDArray.getJSONObject(index);
+                            IDmenuitems = IDorder.getJSONArray("menuItems");
+                            //IDmenuitems = IDfoundObj.getJSONArray("menuItems");
                             //
-                            Log.d("menuItems Obj", menuitemobjs.toString());
+                            Log.d("ID order Obj", IDorder.toString());
+                            Log.d("ID menuitem array", IDmenuitems.toString());
                             //
-                            oitemname = menuitemobjs.get("name").toString();
-                            oitemprice = menuitemobjs.get("price").toString();
-                            //
-                            Log.d("oitemname",oitemname);
-                            Log.d("oitemprice",oitemprice);
-                            // add value to each key
-                            order_tmpmap.put("name", oitemname);
-                            order_tmpmap.put("price", oitemprice);
 
-                            // add order_tmpmap to sbasket master list
-                            sbasketlist.add(order_tmpmap);
-                        }
-                    }
+                            for (int j = 0; j < IDmenuitems.length(); j++) {
+                                JSONObject menuitemobjs = IDmenuitems.getJSONObject(j);
+                                //
+                                Log.d("menuItems Obj", menuitemobjs.toString());
+                                //
+                                oitemname = menuitemobjs.get("name").toString();
+                                oitemprice = menuitemobjs.get("price").toString();
+                                //
+                                Log.d("oitemname", oitemname);
+                                Log.d("oitemprice", oitemprice);
+                                // add value to each key
+                                order_tmpmap.put("name", oitemname);
+                                order_tmpmap.put("price", oitemprice);
+                                // refresh total price of the order
 
+                                // add order_tmpmap to sbasket master list
+                                sbasketlist.add(order_tmpmap);
+                                //
+                                Log.d("sbasketlist", sbasketlist.toString());
+                            }
+//                        }
+//                    }
                 } catch (JSONException e) {
                     e.printStackTrace();
-                }
+                } // catch JSONException here
+
             } else {
                 Log.e("ServiceHandler", "Couldn't get any data from the url");
             }
@@ -196,16 +214,6 @@ public class stafforderactivity extends ListActivity {
                     new int[]{R.id.foodname, R.id.foodprice}
 
             );
-//            procedded = (Button)findViewById(R.id.proceed_button);
-//
-//            procedded.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//
-//                }
-//            });
-            // set on click action for fab2goback
-
 
             setListAdapter(adapter);
         }
